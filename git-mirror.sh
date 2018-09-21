52c8a98da3eb1af52dc371366ab00947cb0a32ac
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-09-21 15:05:47 +0530

    Create git-mirror.sh

---
+++ git-mirror.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Git Add Config 
+git config --global credential.helper '!aws codecommit credential-helper $@'
+git config --global credential.UseHttpPath true
+
+# Git Hub Repository 
+git clone --mirror https://github.com/vinodibs/Automations-Scripts.git replica
+cd replica/
+
+# Code Commit Repository
+git remote add sync https://git-codecommit.us-east-1.amazonaws.com/v1/repos/codecommit
+
+# Sync Between Git Hub & Code Commit Repository
+git fetch -p origin
+git push sync --mirror
+
+
+# * * * * * cd ~/tmp/replica && git fetch -p origin && git push sync --mirror
