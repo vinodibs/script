6fdc586ec57a5c2309bc3ba57166341391c6b573
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-09-21 15:09:33 +0530

    Update git-mirror.sh

--- git-mirror.sh
+++ git-mirror.sh
@@ -6,9 +6,9 @@ git config --global credential.UseHttpPath true
 
 # Git Hub Repository 
 git clone --mirror https://github.com/vinodibs/Automations-Scripts.git replica
-cd replica/
 
 # Code Commit Repository
+cd replica/
 git remote add sync https://git-codecommit.us-east-1.amazonaws.com/v1/repos/codecommit
 
 # Sync Between Git Hub & Code Commit Repository
