2f9054bd3d031b8af7fd843aa41a70e6c2131841
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-11-01 20:28:19 +0530

    Add files via upload

---
+++ http_200.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+Http_Response=`curl -I https://www.google.com/ --write-out %{http_code} --connect-timeout 3 --max-time 5 2>/dev/null | awk '{print $2}' |cut -d "G" -f1|head -1`
+if [ $Http_Response == 200 ]
+then
+   echo "Site is up and Running "
+else
+   echo "Site is down Restart service"
+   service httpd restart
+   service mysqld stop
+   service mysql start
+fi
