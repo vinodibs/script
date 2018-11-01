cb54a3f63f9124e00e7acd798d3947dc30816a65
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-11-01 20:32:15 +0530

    Update http_200.sh

--- http_200.sh
+++ http_200.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+# Add Crontab  * * * * *        /opt/http_200.sh
 Http_Response=`curl -I https://www.google.com/ --write-out %{http_code} --connect-timeout 3 --max-time 5 2>/dev/null | awk '{print $2}' |cut -d "G" -f1|head -1`
 if [ $Http_Response == 200 ]
 then
