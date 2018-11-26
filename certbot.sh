b92cddd7945d7da069d99ec5cfac475d8dc971b7
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-11-26 15:47:35 +0530

    Update certbot.sh

--- certbot.sh
+++ certbot.sh
@@ -2,4 +2,4 @@
 set -x
 git clone https://github.com/certbot/certbot.git
 cd certbot/
-sudo ./certbot-auto --debug -v --server https://acme-v01.api.letsencrypt.org/directory certonly -d abc.com
+sudo ./certbot-auto --debug -v --email abc@gmail.com --server https://acme-v01.api.letsencrypt.org/directory certonly -d abc.com -d www.abc.com -d *.abc.com
