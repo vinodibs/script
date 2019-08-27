2346f0bcae14a28736662971c7e7429bc611563a
vinodibs <32012530+vinodibs@users.noreply.github.com>
2019-08-27 20:08:03 +0530

    Update certbot.sh

--- certbot.sh
+++ certbot.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -x
+read email-ID
+read domainName
+sudo apt-get update -y && apt-get install git -y 
 git clone https://github.com/certbot/certbot.git
 cd certbot/
-sudo ./certbot-auto --debug -v --email abc@gmail.com --server https://acme-v01.api.letsencrypt.org/directory certonly -d abc.com -d www.abc.com -d *.abc.com
+#sudo ./certbot-auto --debug -v --email abc@gmail.com --server https://acme-v01.api.letsencrypt.org/directory certonly -d abc.com -d www.abc.com -d *.abc.com
+sudo ./certbot-auto --debug -v --email ${email-ID} --server https://acme-v01.api.letsencrypt.org/directory certonly -d ${domainName}
